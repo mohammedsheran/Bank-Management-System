077726e323093816ec03495e361d801a36678fca
Mohammed Sheran <mohammed.sh.hawas01@gmail.com>
2026-08-23 11:54:20 +0300

    Implement transaction option selection

--- BankManagementSystem/TransactionsScreen.cpp
+++ BankManagementSystem/TransactionsScreen.cpp
@@ -1,6 +1,7 @@
 
 #include "TransactionsScreen.h"
 #include "Libraries/Utils.h"
+#include "Libraries/InputUtils.h"
 
 void TransactionsScreen::ShowTransactionsMenuScreen()
 {
@@ -14,4 +15,10 @@ void TransactionsScreen::ShowTransactionsMenuScreen()
         << Utils::Tab(3) << "[4] Main Menu.\n\n";
 
     cout << Utils::Divider(110) << '\n';
+}
+
+TransactionsScreen::TransactionsMenuOptions TransactionsScreen::_ReadTransactionsMenuOptions()
+{
+    short option{ InputUtils::ReadNumberInRange("Choose what do you want to do [1-8]: ", static_cast<short>(1), static_cast<short>(4)) };
+    return static_cast<TransactionsMenuOptions>(option);
 }
--- BankManagementSystem/TransactionsScreen.h
+++ BankManagementSystem/TransactionsScreen.h
@@ -4,6 +4,16 @@
 
 class TransactionsScreen : public Screen
 {
+private:
+	enum class TransactionsMenuOptions
+	{
+		BalanceList = 1,
+		Deposit,
+		Withdraw,
+		MainMenu
+	};
+
+	static TransactionsMenuOptions _ReadTransactionsMenuOptions();
 public:
 	static void ShowTransactionsMenuScreen();
 };
