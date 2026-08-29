a65e1fddf5eefc700fbe1fe6d1864ee9f85a61a1
Mohammed Sheran <166550446+mohammedsheran@users.noreply.github.com>
2026-08-29 17:51:08 +0300

    Implement Currency entity (#31)
    * Set up Currency class
    
    * Implement Currency entity
    
    * Implement Currency serialization
    
    * Implement currency data loading
    
    * Implement currency lookup
    
    * Implement currency rate update
    
    * Implement GetCurrencyList

---
+++ BankManagementSystem/Currency.cpp
@@ -0,0 +1,226 @@
+
+#include "Currency.h"
+#include "Libraries/StringUtils.h"
+
+#include <stdexcept>
+#include <fstream>
+
+std::string Currency::_RequireField(const std::string& str)
+{
+	if (!str.empty())
+	{
+		return str;
+	}
+
+	throw std::invalid_argument("Field can not be empty.");
+}
+
+float Currency::_ValidateRate(const float& rate)
+{
+	if (rate >= 0)
+	{
+		return rate;
+	}
+
+	throw std::invalid_argument("Field can not be less than zero.");
+}
+
+Currency::Currency(const Mode& mode) : _mode(mode) {}
+
+Currency::Currency(const Mode& mode, const std::string& country, const std::string& code, const std::string& name, const float& rate)
+	: _mode(mode), _country(_RequireField(country)), _code(_RequireField(code)), _name(_RequireField(name)), _rate(_ValidateRate(rate)){ }
+
+std::string Currency::GetCountry() const
+{
+	return _country;
+}
+
+std::string Currency::GetCode() const
+{
+	return _code;
+}
+
+std::string Currency::GetName() const
+{
+	return _name;
+}
+
+void Currency::UpdateRate(const float& rate)
+{
+	_rate = _ValidateRate(rate);
+	_UpdateCurrency();
+}
+
+float Currency::GetRate() const
+{
+	return _rate;
+}
+
+bool Currency::IsEmpty() const
+{
+	return _mode == Mode::Empty;
+}
+
+Currency Currency::_ConvertLineToCurrencyObject(const std::string& line, const std::string& delimiter)
+{
+	std::vector <std::string> vString{ StringUtils::Split(line, delimiter) };
+
+	if (vString.size() != 4)
+	{
+		return Currency(Mode::Empty);
+	}
+
+	return Currency(Mode::Update, vString[0], vString[1], vString[2], stof(vString[3]));
+}
+
+std::string Currency::_ConvertCurrencyObjectToLine(const Currency& currency, const std::string& separator)
+{
+	if (currency.IsEmpty())
+	{
+		return {};
+	}
+
+	std::string line{};
+
+	line += currency._country + separator;
+	line += currency._code + separator;
+	line += currency._name + separator;
+	line += to_string(currency._rate);
+
+	return line;
+}
+
+std::vector <Currency> Currency::_LoadCurrencies()
+{
+	std::vector <Currency> vCurrencies{};
+
+	std::ifstream file{ "Currencies.txt" };
+
+	if (!file)
+	{
+		cout << "Failed to open file: Currencies.txt\n";
+		return{};
+	}
+
+	std::string line{};
+
+	while (getline(file, line))
+	{
+		vCurrencies.push_back(_ConvertLineToCurrencyObject(line));
+	}
+
+	file.close();
+
+	return vCurrencies;
+}
+
+bool Currency::_SaveCurrencies(const std::vector <Currency>& vCurrencies)
+{
+	if (vCurrencies.empty())
+	{
+		return {};
+	}
+
+	std::ofstream file{ "Currencies.txt" };
+
+	if (!file)
+	{
+		cout << "Failed to open file: Currencies.txt\n";
+		return {};
+	}
+
+	for (const auto& currency : vCurrencies)
+	{
+		file << _ConvertCurrencyObjectToLine(currency) << '\n';
+	}
+
+	file.close();
+	return true;
+}
+
+Currency Currency::FindByCountry(std::string country)
+{
+	std::ifstream file{ "Currencies.txt" };
+
+	if (!file)
+	{
+		cout << "Failed to open file: Currencies.txt\n";
+		return Currency(Mode::Empty);
+	}
+
+	country = StringUtils::ToUpper(country);
+
+	std::string line{};
+
+	while (getline(file, line))
+	{
+		Currency currency{ _ConvertLineToCurrencyObject(line) };
+
+		if (StringUtils::ToUpper(currency.Country) == country)
+		{
+			file.close();
+			return currency;
+		}
+	}
+
+	file.close();
+
+	return Currency(Mode::Empty);
+}
+
+Currency Currency::FindByCode(std::string code)
+{
+	std::ifstream file{ "Currencies.txt" };
+
+	if (!file)
+	{
+		cout << "Failed to open file: Currencies.txt\n";
+		return Currency(Mode::Empty);
+	}
+
+	code = StringUtils::ToUpper(code);
+
+	std::string line{};
+
+	while (getline(file, line))
+	{
+		Currency currency{ _ConvertLineToCurrencyObject(line) };
+
+		if (StringUtils::ToUpper(currency.Code) == code)
+		{
+			file.close();
+			return currency;
+		}
+	}
+
+	file.close();
+
+	return Currency(Mode::Empty);
+}
+
+bool Currency::IsCurrecnyExists(const std::string& code)
+{
+	Currency currency{ Currency::FindByCode(code) };
+	return !currency.IsEmpty();
+}
+
+bool Currency::_UpdateCurrency()
+{
+	std::vector <Currency> vCurrencies{ _LoadCurrencies() };
+
+	for (auto& currency : vCurrencies)
+	{
+		if (currency.Code == Code)
+		{
+			currency = *this;
+			break;
+		}
+	}
+
+	return _SaveCurrencies(vCurrencies);
+}
+
+std::vector <Currency> Currency::GetCurrecnyList()
+{
+	return _LoadCurrencies();
+}
---
+++ BankManagementSystem/Currency.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+class Currency
+{
+private:
+	enum class Mode
+	{
+		Empty = 1,
+		Update
+	};
+
+	Mode _mode{};
+	std::string _country{};
+	std::string _code{};
+	std::string _name{};
+	float _rate{};
+
+	static std::string _RequireField(const std::string& str);
+	
+	static float _ValidateRate(const float& rate);
+
+	static Currency _ConvertLineToCurrencyObject(const std::string& line, const std::string& delimiter = "#:#");
+	static std::string _ConvertCurrencyObjectToLine(const Currency& currency, const std::string& separator = "#:#");
+
+	static std::vector <Currency> _LoadCurrencies();
+	static bool _SaveCurrencies(const std::vector <Currency>& vCurrencies);
+
+	bool _UpdateCurrency();
+
+public:
+	Currency(const Mode& mode);
+	Currency(const Mode& mode, const std::string& country, const std::string& code, const std::string& name, const float& rate);
+
+	std::string GetCountry() const;
+	std::string GetCode() const;
+	std::string GetName() const;
+
+	void UpdateRate(const float& rate);
+	float GetRate() const;
+
+	__declspec(property(get = GetCountry)) std::string Country;
+	__declspec(property(get = GetCode)) std::string Code;
+	__declspec(property(get = GetName)) std::string Name;
+	__declspec(property(get = GetRate)) float Rate;
+
+	bool IsEmpty() const;
+
+	static Currency FindByCountry(std::string country);
+	static Currency FindByCode(std::string code);
+
+	static bool IsCurrecnyExists(const std::string& code);
+
+	static std::vector <Currency> GetCurrecnyList();
+
+};
