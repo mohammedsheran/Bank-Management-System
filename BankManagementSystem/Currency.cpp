f56619ebdf43911c2e96ab1cc878258dddfd180f
Mohammed Sheran <mohammed.sh.hawas01@gmail.com>
2026-08-29 17:07:29 +0300

    Implement Currency entity

--- BankManagementSystem/Currency.cpp
+++ BankManagementSystem/Currency.cpp
@@ -1,1 +1,58 @@
+
 #include "Currency.h"
+#include <stdexcept>
+
+std::string Currency::_RequireField(const std::string& str)
+{
+	if (!str.empty())
+	{
+		return str;
+	}
+
+	throw std::invalid_argument("Field can not be empty.");
+}
+
+float Currency::_ValidateRate(const float& rate)
+{
+	if (rate >= 0)
+	{
+		return rate;
+	}
+
+	throw std::invalid_argument("Field can not be less than zero.");
+}
+
+Currency::Currency(const Mode& mode) : _mode(mode) {}
+
+Currency::Currency(const Mode& mode, const std::string& country, const std::string& code, const std::string& name, const float& rate)
+	: _mode(mode), _country(_RequireField(country)), _code(_RequireField(code)), _name(_RequireField(name)), _rate(_ValidateRate(rate)){ }
+
+std::string Currency::GetCountry() const
+{
+	return _country;
+}
+
+std::string Currency::GetCode() const
+{
+	return _code;
+}
+
+std::string Currency::GetName() const
+{
+	return _name;
+}
+
+void Currency::UpdateRate(const float& rate)
+{
+	_rate = _ValidateRate(rate);
+}
+
+float Currency::GetRate() const
+{
+	return _rate;
+}
+
+bool Currency::IsEmpty() const
+{
+	return _mode == Mode::Empty;
+}
--- BankManagementSystem/Currency.h
+++ BankManagementSystem/Currency.h
@@ -1,5 +1,42 @@
 #pragma once
 
+#include <string>
+
 class Currency
 {
+private:
+	enum class Mode
+	{
+		Empty = 1,
+		Update
+	};
+
+	Mode _mode{};
+	std::string _country{};
+	std::string _code{};
+	std::string _name{};
+	float _rate{};
+
+	static std::string _RequireField(const std::string& str);
+	
+	static float _ValidateRate(const float& rate);
+
+public:
+	Currency(const Mode& mode);
+	Currency(const Mode& mode, const std::string& country, const std::string& code, const std::string& name, const float& rate);
+
+	std::string GetCountry() const;
+	std::string GetCode() const;
+	std::string GetName() const;
+
+	void UpdateRate(const float& rate);
+	float GetRate() const;
+
+	__declspec(property(get = GetCountry)) std::string Country;
+	__declspec(property(get = GetCode)) std::string Code;
+	__declspec(property(get = GetName)) std::string Name;
+	__declspec(property(get = GetRate)) float Rate;
+
+	bool IsEmpty() const;
+
 };
