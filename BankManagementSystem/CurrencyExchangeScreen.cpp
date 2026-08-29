23e5efc7ac5b1a670349b863b4c0caeea1219a5e
Mohammed Sheran <166550446+mohammedsheran@users.noreply.github.com>
2026-08-29 19:36:19 +0300

    Implement Currency Exchange Screen (#32)
    * Set up CurrencyExchangeScreen class
    
    * Implement currency exchange menu display
    
    * Implement currency exchange option selection
    
    * Implement currency exchange menu navigation
    
    * Connect currency exchange  menu to screens

---
+++ BankManagementSystem/CurrencyExchangeScreen.cpp
@@ -0,0 +1,94 @@
+
+#include "CurrencyExchangeScreen.h"
+#include "Libraries/Utils.h"
+#include "Libraries/InputUtils.h"
+
+void CurrencyExchangeScreen::ShowCurrencyExchangeMenuScreen()
+{
+	Screen::ClearScreen();
+
+	if (!Screen::CheckAccessRight(User::UserPermissions::Transactions))
+	{
+		//Logger::Warning(LogMessages::accessDenied + " | Username: " + currentUser.Username + " | Operation: Transactions");
+		return;
+	}
+
+	Screen::ShowScreenHeader("Currency Exchange Screen");
+
+	cout << Utils::Tab(4) << "[1] Currency List."
+		<< Utils::Tab(2) << "[3] Update Rate.\n";
+
+	cout << Utils::Tab(4) << "[2] Find Currency."
+		<< Utils::Tab(2) << "[5] Main Menu.\n";
+
+	cout << Utils::Tab(4) << "[4] Currency Calculator.\n\n";
+
+	cout << Utils::Divider(110) << '\n';
+
+	_HandleCurrencyExchangeMenuOption(_ReadCurrencyExchangeMenuOption());
+}
+
+CurrencyExchangeScreen::CurrencyMenuOptions CurrencyExchangeScreen::_ReadCurrencyExchangeMenuOption()
+{
+	short option{ InputUtils::ReadNumberInRange("Choose what do you want to do [1-5]: ", static_cast<short>(1), static_cast<short>(5)) };
+	return static_cast<CurrencyMenuOptions>(option);
+}
+
+void CurrencyExchangeScreen::_ReturnToCurrencyExchangeMenu()
+{
+	Screen::PauseScreen("Press any key to return to currency exchange menu...");
+
+	ShowCurrencyExchangeMenuScreen();
+}
+
+void CurrencyExchangeScreen::_HandleCurrencyExchangeMenuOption(CurrencyMenuOptions option)
+{
+	switch (option)
+	{
+	case CurrencyExchangeScreen::CurrencyMenuOptions::CurrencyList:
+		Screen::ClearScreen();
+		_ShowCurrencyListScreen();
+		break;
+
+	case CurrencyExchangeScreen::CurrencyMenuOptions::FindCurrency:
+		Screen::ClearScreen();
+		_ShowFindCurrencyScreen();
+		break;
+	
+	case CurrencyExchangeScreen::CurrencyMenuOptions::UpdateRate:
+		Screen::ClearScreen();
+		_ShowUpdateRateScreen();
+		break;
+
+	case CurrencyExchangeScreen::CurrencyMenuOptions::CurrencyCalculator:
+		Screen::ClearScreen();
+		_ShowCurrencyCalculatorScreen();
+		break;
+
+	case CurrencyExchangeScreen::CurrencyMenuOptions::MainMenu:
+		return;
+	}
+
+	_ReturnToCurrencyExchangeMenu();
+}
+
+void CurrencyExchangeScreen::_ShowCurrencyListScreen()
+{
+	std::cout << "Currency List Scrren";
+}
+
+void CurrencyExchangeScreen::_ShowFindCurrencyScreen()
+{
+	std::cout << "Find Currency Scrren";
+
+}
+
+void CurrencyExchangeScreen::_ShowUpdateRateScreen()
+{
+	std::cout << "Update Rate Scrren";
+}
+
+void CurrencyExchangeScreen::_ShowCurrencyCalculatorScreen()
+{
+	std::cout << "Currency Calculator Scrren";
+}
